ca8855693867db8784d58625ebc66691378e6579
Jake Williams <jakewilliams117@gmail.com>
2019-05-07 15:40:17 -0400

    Updates

---
+++ precache-manifest.bcf18800fa0d8f7e07f63732a1bc7b4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6cdff4927730017bd1ccc026a86c238",
+    "url": "/CarCompanion/index.html"
+  },
+  {
+    "revision": "60c096f770d7f9fb02d4",
+    "url": "/CarCompanion/static/css/main.ed17d071.chunk.css"
+  },
+  {
+    "revision": "ad2b735b9bb0b44eb8a8",
+    "url": "/CarCompanion/static/js/2.1543f537.chunk.js"
+  },
+  {
+    "revision": "60c096f770d7f9fb02d4",
+    "url": "/CarCompanion/static/js/main.609b13ec.chunk.js"
+  },
+  {
+    "revision": "cb5ce2f6b05e1495588e",
+    "url": "/CarCompanion/static/js/runtime~main.e2442403.js"
+  }
+]);
